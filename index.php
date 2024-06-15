3f8d64e3f93957e5fc412b481b41dd38a393fd83
Rujal Tamrakar <38482136+rujal404@users.noreply.github.com>
2024-06-15 19:47:21 +0545

    update

--- index.php
+++ index.php
@@ -125,7 +125,7 @@ include "./php/dbconn.php"
         <div class="box-container">
 
             <div class="box">
-                <h3>Follow Us</h3>
+                <h3>Follow Us On</h3>
                 <a href="https://www.facebook.com"> <i class="fab fa-facebook-f"></i></a>
                 <a href="https://www.twitter.com"><i class="fab fa-twitter"></i></a>
                 <a href="https://www.instagram.com"> <i class="fab fa-instagram"></i></a>
